82a950d6925f3b243eca19fbcdd0192b9d5ea644
myd3paredes <121123510+myd3paredes@users.noreply.github.com>
2022-12-20 12:17:23 -0600

    Update streamlit_app.py
    additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.title("My parents new healthy diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blueberry Oatmea")
+streamlit.text("Kale Spinach & Rocket Smoothie")
+streamlit.text("HArd Boiled Free Range Egg")
+
