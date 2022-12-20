d68eb90a8c858285b7b89003c99406f2b5ca000f
myd3paredes <121123510+myd3paredes@users.noreply.github.com>
2022-12-20 12:19:00 -0600

    Update streamlit_app.py
    emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title("My parents new healthy diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmea")
-streamlit.text("Kale Spinach & Rocket Smoothie")
-streamlit.text("HArd Boiled Free Range Egg")
+streamlit.header("🥣    Breakfast Menu")
+streamlit.text("🥗 Omega 3 & Blueberry Oatmea")
+streamlit.text("🐔 Kale Spinach & Rocket Smoothie")
+streamlit.text("🥑🍞 HArd Boiled Free Range Egg")
 
