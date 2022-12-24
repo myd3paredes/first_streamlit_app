1c90864c55dee87e784039705f81d8819b66967f
myd3paredes <121123510+myd3paredes@users.noreply.github.com>
2022-12-24 15:52:03 -0600

    Update streamlit_app.py
    button fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 If streamlit.button('Get Fruit Load List'):
-    my_cnx  = snowflake.connector.connet(**streamlit.secrets["snowflake"])
+    my_cnx  = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
     
