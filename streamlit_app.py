8adbc7fdf26633aeb4c03fccb1561777241613ca
myd3paredes <121123510+myd3paredes@users.noreply.github.com>
2022-12-24 11:06:55 -0600

    Update streamlit_app.py
    error try else

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,29 +18,25 @@ streamlit.header("🍞🥗 Build your own fruit smoothie 🐔🥣 ")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
 my_fruit_list = my_fruit_list.set_index('Fruit')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
+    except URLError as e:
+      streamlit.error()
+      
 #snowflake
 
 my_cur.execute("SELECT * from fruit_load_list")
@@ -48,11 +44,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
